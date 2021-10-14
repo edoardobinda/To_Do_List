8641fec8fb6b461d1c3d760958ea731610d76afe
edoardobinda <81161437+edoardobinda@users.noreply.github.com>
2021-10-14 16:31:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,111 +3,118 @@ var count = 0;
 function functionlist(){
     
     count ++;
-    
     var textField = document.getElementById("item");
-    
-    var textNode = document.createTextNode(textField.value);
-    // textNode è il testo del nodo, il quale ha come valore ciò che inseriamo nel campo input
-    
+    var textNode = document.createTextNode(textField.value);// textNode is the node title, that has a value corresp.ing to the input we give
+   
+if (textField.value.length !== 0){
+
     var listElements = document.createElement("li");
-
+    var spanElement = document.createElement("span");
     var menu = document.getElementById("menu");// id=menu is the UL where our items will appear
-
 
 // Each listElementss (li) gets an ID made of the "count" number
     
     listElements.setAttribute("id", count);
 
-
 // Resetting the placeholder after each submission
     
     document.getElementById("item").value = '';
 
-
 //Creating BUTTONS
 
-    var DoneButton = document.createElement("button");
+    var DoneButton = document.createElement("div");
 
-    var EditButton = document.createElement("button");
+    var EditButton = document.createElement("div");
 
-    var CancelButton = document.createElement("button");
+    var CancelButton = document.createElement("div");
 
-
+    DoneButton.setAttribute("class", count);
     CancelButton.setAttribute("class", count);
     EditButton.setAttribute("class", count);
 
 //LI ELEMENTS
-    
-    listElements.appendChild(textNode);
+
+    spanElement.appendChild(textNode);
+    listElements.appendChild(spanElement); 
     menu.appendChild(listElements);
     listElements.className = "lielements";
 
-//BUTTONS
+//BUTTONS 
     
 //Green Button  (Select/Deselect)
     
     listElements.appendChild(DoneButton);
     DoneButton.innerHTML = "";
-    DoneButton.setAttribute("class", "check");
+    DoneButton.setAttribute("class", "checksign");
 
         DoneButton.onclick = function(){
-        listElements.classList.toggle("DoneButtonPressed");
+            console.log(spanElement);
+            this.classList.toggle("DoneButtonPressed");
+            spanElement.classList.toggle("DoneButtonPressed");
         }
 
 //Blue Button (Edit Button)
     
     listElements.appendChild(EditButton);
-    EditButton.innerHTML = "EditButton";
-
+    EditButton.classList.add("editsign");
+    
     var buttonSave = document.createElement("button");
     buttonSave.innerHTML = "ButtonSave";
     var target2 = textNode.nodeValue;
     
-        
         EditButton.onclick = function(){
+            buttonSave.setAttribute("id", "prov");
+            if (document.contains(document.getElementById("prov"))){
+                let thisLi = document.getElementById(this.classList[0]);
+                let thisLitext = thisLi.firstChild.textContent;
+                textField.value = thisLitext;
+                document.getElementById("prov").remove();
+                var inputcontainer = document.getElementById("formdiv");
+                buttonSave.setAttribute("class", count);
+                inputcontainer.appendChild(buttonSave);}
             
-            let thisLi = document.getElementById(this.classList);
+            else { 
+            buttonSave.setAttribute("id", "prov");  
+            let thisLi = document.getElementById(this.classList[0]);
             let thisLitext = thisLi.firstChild.textContent;
             textField.value = thisLitext;
                       
             var inputcontainer = document.getElementById("formdiv");
             buttonSave.setAttribute("class", count);
-            inputcontainer.appendChild(buttonSave);
-
-            
-            /*console.log(textNode);*/
-
-            //If uncommmented, this line will allow the "Li" item to persist on the UL list
-            //till the change has been made on the input line.
-            /*textField.onchange = function(){
-            var cancel = document.getElementById(CancelButton.className);
-            cancel.remove();
-            }*/
-            
-    }
+            inputcontainer.appendChild(buttonSave);}
+            }
     
-// BUTTONSAVE 
+//BUTTONSAVE (update the current Li text)
 
     buttonSave.onclick = function(){
         let newValue = textField.value;
-        
         textNode.nodeValue = newValue;
         this.remove();
         document.getElementById("item").value = '';
-
     };
 
 //Red Button (Cancel Entry Button)
-    
+      
     listElements.appendChild(CancelButton);
-    CancelButton.innerHTML = "CancelButton";
-        
+    CancelButton.classList.add("cancelsign");
+    
         CancelButton.onclick = function(){
-        var cancel = document.getElementById(CancelButton.className);
+        buttonSave.remove();     
+        var cancel = document.getElementById(CancelButton.classList[0]);
         cancel.remove();
+        document.getElementById("prov").remove();
+        document.getElementById("item").value = '';
         }
+
+}
+else {
+    alert("Input cannot be blank!");
+}
 }
 
+// CLEAR FUNCTION TO ERASE PRESENT TO-DO-LIST
 function functionclear(){
     document.getElementById("menu").innerHTML = "";
+    document.getElementById("item").value = '';
+    document.getElementById("prov").remove();
 }
